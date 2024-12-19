2a4ea67b58531e113c79244a4e29401a6c295eab
Ashutosh Pathak <ashu.p89@gmail.com>
2024-12-19 14:24:59 +0530

    OpenFeign removed, RestClient successfully used

--- order-service/src/main/java/com/ashu/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/ashu/microservices/order/client/InventoryClient.java
@@ -1,13 +1,12 @@
 package com.ashu.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(name = "inventory", url = "http://localhost:8082")
+//@FeignClient(name = "inventory", url = "http://localhost:8082")
 public interface InventoryClient  {
 
-    @GetMapping("/api/inventory")
+//    @GetMapping("/api/inventory")
+    @GetExchange("/api/inventory")
     boolean checkInventory(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
