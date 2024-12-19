d7079372d887a64b89eb16d45539abbd4762f336
Ashutosh Pathak <ashu.p89@gmail.com>
2024-12-19 18:48:43 +0530

    done as per video. but circuit breaker is not breaking :) not going to open state. working on fixing it.

--- order-service/src/main/java/com/ashu/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/ashu/microservices/order/client/InventoryClient.java
@@ -1,12 +1,27 @@
 package com.ashu.microservices.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 //@FeignClient(name = "inventory", url = "http://localhost:8082")
+
 public interface InventoryClient  {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
 
 //    @GetMapping("/api/inventory")
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean checkInventory(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skuCode: {}, failure reason: {}", skuCode, throwable.getMessage());
+        return false;
+    }
 }
